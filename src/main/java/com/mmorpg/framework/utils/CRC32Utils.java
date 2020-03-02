b351a6784a1d8f8f2c65a6fc1f9c5d424abc6512
Ariescat <1275563227@qq.com>
2020-03-02 17:55:24 +0800

    feat(packet): CRC32

--- src/main/java/com/mmorpg/framework/utils/CRC32Utils.java
+++ src/main/java/com/mmorpg/framework/utils/CRC32Utils.java
@@ -1,15 +1,27 @@
 package com.mmorpg.framework.utils;
 
+import java.util.zip.CRC32;
+
 /**
  * @author Ariescat
  * @version 2020/3/2 17:50
  */
 public class CRC32Utils {
-	public static long calCRC32(byte[] array, int index, int length) {
-		return 0;
+
+	private static final ThreadLocal<CRC32> crc32 = new ThreadLocal<CRC32>() {
+		@Override
+		protected CRC32 initialValue() {
+			return new CRC32();
+		}
+	};
+
+	public static long calCRC32(byte[] array, int off, int length) {
+		CRC32 crc32 = CRC32Utils.crc32.get();
+		crc32.update(array, off, length);
+		return crc32.getValue();
 	}
 
 	public static long calCRC32(byte[] array) {
-		return 0;
+		return calCRC32(array, 0, array.length);
 	}
 }
